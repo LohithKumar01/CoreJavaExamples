40919c22cec6d88088fd59f211ee331a9aa7fed1
LohithKumar01 <62865353+LohithKumar01@users.noreply.github.com>
2023-07-15 21:16:08 -0500

    New Revise

--- src/Main.java
+++ src/Main.java
@@ -66,8 +66,41 @@
 //    }
 //}
 
+//public class Main {
+//    public static void main(String[] args) {
+//        String str="University of North Texas";
+//        String[] stringSplit = str.split(" ");
+//        int arraylength = stringSplit.length;
+//        System.out.println(stringSplit[arraylength-1]);
+//    }
+//}
+
 public class Main {
     public static void main(String[] args) {
+        int highScorePosition=calculateHighScorePosition(1500);
+        displayHighScorePosition("Kim",highScorePosition);
+    }
 
+//    public static void calculateSCore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//        int finalScore = score;
+//        if (gameOver == true) {
+//            finalScore += (levelCompleted * bonus);
+//            System.out.println("Your final score is " + finalScore);
+//        }
+//    }
+    public static void displayHighScorePosition(String playerName,int playerPosition){
+        System.out.println(playerName + " managed to get into position "+playerPosition+" on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore >=1000){
+            return 1;
+        } else if ((playerScore >= 500)&&(playerScore<1000)) {
+            return 2;
+        } else if ((playerScore >= 100) && (playerScore < 500)) {
+            return 3;
+        }else {
+            return 4;
+        }
     }
 }
